de8ee3f8a591b75ec22c4d39f0a7cd058d710e35
huubang123 <154033079+huubang123@users.noreply.github.com>
2025-12-02 15:34:46 +0700

    update

--- Draw.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-void DrawBoard(int x, int y, int width, int height);
-void Draw(const char* str);
--- System.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <windows.h>
-
-void GotoXY(int x, int y);
-void ShowCursor(bool show);
-void PauseGame(HANDLE t);
-void ExitGame(HANDLE t);
